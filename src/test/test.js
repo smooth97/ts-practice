a99b71481653058ea0791a42f00a1e84506b537a
smooth97 <dbtmdwls0506@naver.com>
2020-05-02 17:59:35 +0900

    basic

--- src/test/test.js
+++ src/test/test.js
@@ -1,14 +1,36 @@
 "use strict";
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
 function sum(x, y) {
-  return x + y;
+    return x + y;
 }
 sum(1, 2);
 function sumArray(numbers) {
-  return numbers.reduce(function (acc, current) {
-    return acc + current;
-  }, 0);
+    return numbers.reduce(function (acc, current) { return acc + current; }, 0);
 }
 var total = sumArray([1, 2, 3, 4, 5]);
 function returnNothing() {
-  console.log("I am just saying hello world");
+    console.log("I am just saying hello world");
 }
+var person = {
+    name: "김사람",
+    age: 20,
+};
+var expert = {
+    name: "김개발",
+    skills: ["javascript", "react"],
+};
+var people = [person, expert];
+function merge(a, b) {
+    return __assign(__assign({}, a), b);
+}
+var merged = merge({ foo: 1 }, { bar: 1 });
